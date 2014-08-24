1c95947731bb0852cdc5af83328e1d57a7c25c91
Jmcus <vluxyue@gmail.com>
2014-08-24 20:56:33 +0800

    add the hello.js

--- microblog/app.js
+++ microblog/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var hello = require('./routes/hello');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/hello', hello);            //新加
+
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
